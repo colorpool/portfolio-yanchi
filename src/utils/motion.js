98470f6ba3394aea4a1e73f14a89bda75224d66a
Colorpool <55055121+colorpool@users.noreply.github.com>
2024-02-29 18:06:02 -0500

    Initial commit

---
+++ src/utils/motion.js
@@ -0,0 +1,20 @@
+export const transition = {type: "spring", duration: 0.8};
+export const headContentAnimation = {
+  initial: {y: 100, opacity: 0},
+  animate: {y: 0, opacity: 1},
+  transition: {
+    type: "spring",
+    damping: 7,
+    stiffness: 30,
+    restDelta: 0.001,
+    duration: 0.6,
+    delay: 0.2,
+    delayChildren: 0.2,
+  },
+};
+
+export const headContainerAnimation = {
+  initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
+  animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
+  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+};
